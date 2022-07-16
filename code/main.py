99a9ad1ec91ae13cb6376b075a40becbee0849e8
Guy Nithikrit Raibrochareon <guy.nithi@gmail.com>
2022-07-16 07:24:12 +0700

    level object

--- code/main.py
+++ code/main.py
@@ -1,6 +1,31 @@
 import pygame, sys
 from settings import *
 from player import Player
+from car import Car
+from random import choice, randint
+from sprite import SimpleSprite, LongSprite
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.bg = pygame.image.load('./graphics/main/map.png').convert()
+        self.fg = pygame.image.load('./graphics/main/overlay.png').convert_alpha()
+
+    def customize_draw(self):
+
+        # Change the offset vector
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        # blit the bg
+        display_surface.blit(self.bg, -self.offset)
+
+        for sprite in sorted(self.sprites(),key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            display_surface.blit(sprite.image, offset_pos)
+
+        display_surface.blit(self.fg,-self.offset)
 
 # Basic setup
 pygame.init()
@@ -9,10 +34,27 @@ pygame.display.set_caption("Frogger")
 clock = pygame.time.Clock()
 
 # Groups
-all_sprites = pygame.sprite.Group()
+all_sprites = AllSprites()
 
 # Sprites
 player = Player((600,400),all_sprites)
+
+# Timer
+car_timer = pygame.event.custom_type()
+pygame.time.set_timer(car_timer,50)
+pos_list = []
+
+# Sprite setup
+for file_name, pos_list in SIMPLE_OBJECTS.items():
+    path = f'./graphics/objects/simple/{file_name}.png'
+    surf = pygame.image.load(path).convert_alpha()
+    for pos in pos_list:
+        SimpleSprite(surf,pos,all_sprites)
+
+for file_name, pos_list in LONG_OBJECTS.items():
+    surf = pygame.image.load(f'./graphics/objects/long/{file_name}.png').convert_alpha()
+    for pos in pos_list:
+        LongSprite(surf,pos,all_sprites)
 
 # Game loop
 while True:
@@ -21,6 +63,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == car_timer:
+            random_pos = choice(CAR_START_POSITIONS)
+            if random_pos not in pos_list:
+                pos_list.append(random_pos)
+                pos = (random_pos[0],random_pos[1] + randint(-8,8))
+                Car(pos,all_sprites)
+            if len(pos_list) > 5:
+                del pos_list[0]
 
     # Delta time
     dt = clock.tick() / 1000
@@ -32,7 +82,8 @@ while True:
     all_sprites.update(dt)
 
     # Draw
-    all_sprites.draw(display_surface)
+    # all_sprites.draw(display_surface)
+    all_sprites.customize_draw()
 
     # Update the display_surface -> drawing the frame
     pygame.display.update()
--- code/player.py
+++ code/player.py
@@ -1,11 +1,16 @@
 import pygame
+from os import walk
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
+
+        # Image
         self.import_asset()
         self.frame_index = 0
-        self.image = self.animation[0]
+        self.status = 'up'
+        # self.image = self.animation[self.frame_index]
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center = pos)
 
         # Float based movement
@@ -14,10 +19,27 @@ class Player(pygame.sprite.Sprite):
         self.speed = 200
 
     def import_asset(self):
-        path = './graphics/player/right/'
-        self.animation = [pygame.image.load(f'{path}{frame}.png').convert_alpha() for frame in range(4)]
+        self.animations = {}
+        for index, folder in enumerate(walk('./graphics/player')):
+            # print(f"Folder: {folder}")
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in folder[2]:
+                    # print(file_name, folder[2])
+                    path = folder[0].replace('\\','/') + '/' + file_name 
+                    # print(f"Path: {path}")
+                    surf = pygame.image.load(path).convert_alpha()
+                    # print(f"Folder[0]: {folder[0]}")
+                    # key = folder[0].split('\\')[1]
+                    key = folder[0].split('/')[3]
+                    # print(f"Key: {key}")
+                    self.animations[key].append(surf)
 
     def move(self,dt):
+
+        # normalize a vector -> the length of a vector is going to be 1
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
@@ -26,20 +48,39 @@ class Player(pygame.sprite.Sprite):
 
     def input(self):
         keys = pygame.key.get_pressed()
+
+        # Horizontal input
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
 
+        # Vertical Input
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
+
+    def animate(self,dt):
+        current_animation = self.animations[self.status]
+
+        if self.direction.magnitude() != 0:
+            self.frame_index += 10 * dt
+            if self.frame_index >= len(current_animation):
+                self.frame_index = 0
+        else:
+            self.frame_index = 0
+        self.image = current_animation[int(self.frame_index)]
 
     def update(self,dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
